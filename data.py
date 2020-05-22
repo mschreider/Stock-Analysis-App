0add4f5682157247c926dc6efac0416f541c35e8
Matt Schreider <matt.schreider@gmail.com>
2020-05-21 23:46:48 -0400

    Implemented Upper and Lower Bollinger Bands, Plotted Bollinger Bands

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import pandas, json
+import pandas as pd
 import numpy as np
 from matplotlib import pyplot, axes
 from matplotlib import dates as mdates
@@ -18,6 +18,53 @@ class Stock():
         df = data.DataReader(name=self.ticker, data_source="yahoo",start=start,end=end)
         df.index.to_pydatetime()
         return df
+    
+    def getSMA(self):
+        df = self.getData()
+        closeprice = df['Close']
+        time_period = 20    
+        """
+        numbers_series = pd.Series(closeprice)
+        windows = numbers_series.rolling(time_period)
+        moving_averages = windows.mean()
+        moving_averages_list = moving_averages.tolist()
+        df['20-day SMA'] = moving_averages_list
+        """
+        df['20-day SMA'] = df['Close'].rolling(time_period).mean()
+
+        return df
+    
+    def getUpperBand(self, df):
+        sma = df['20-day SMA'].tolist()
+        time_period = 20
+        """
+        numbers_series = pd.Series(sma)
+        std_dev = numbers_series.rolling(time_period).std()  
+        std_dev_list = std_dev.tolist()
+        df['20-Day Standard Deviation'] = std_dev_list
+        
+        upper = sma + df['Close'].rolling(time_period).std() * 2
+        df['upperband'] = upper
+        print(upper)
+        """
+        df['Upper Band'] = df['20-day SMA'] + df['Close'].rolling(time_period).std() * 2
+        return df 
+
+    def getLowerBand(self, df):
+        sma = df['20-day SMA'].tolist()
+        time_period = 20
+        """
+        numbers_series = pd.Series(sma)
+        std_dev = numbers_series.rolling(time_period).std()  
+        std_dev_list = std_dev.tolist()
+        df['20-Day Standard Deviation'] = std_dev_list
+        
+        lower = sma - df['Close'].rolling(time_period).std() * 2
+        df['lowerband'] = lower
+        print(lower)
+        """
+        df['Lower Band'] = df['20-day SMA'] - df['Close'].rolling(time_period).std() * 2
+        return df 
 
     def inc_dec(self, c, o):
         if c > o:
@@ -52,7 +99,19 @@ class Stock():
         fig = pyplot.figure(figsize=(10, 7))
         ax1 = pyplot.subplot()
         candlestick_ochl(ax1, data, width=np.timedelta64(12, 'h'), colorup='green', colordown='red', alpha=0.75)
-        # pyplot.plot(date, high, 'go', date, low, 'ro')
+        ## Plot 20-Day SMA overlapping Candlestick plot ##
+        sma_plot = pyplot.subplot()
+        pyplot.plot(df['20-day SMA'], color='#26baee', linestyle='--')  
+
+        ## Plot Upper Bands ##
+        upper_plot = pyplot.subplot()
+        pyplot.plot(df['Upper Band'], color='#783eff', linestyle='-')
+        
+        ## Plot Lower Bands ##
+        lower_plot = pyplot.subplot()
+        pyplot.plot(df['Lower Band'], color='#783eff', linestyle='-')
+
+        ## Set Axis properties ##
         pyplot.yticks(np.arange(min(closeprice)-(0.1*min(closeprice)), max(closeprice)+(0.1*max(closeprice)),  50))
         pyplot.ylabel('Price')
         ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
@@ -60,7 +119,8 @@ class Stock():
         pyplot.xlabel('Date')
         pyplot.show()
         
-        
 tesla = Stock('TSLA')
-data = tesla.getData()
+data = tesla.getSMA()
+upper = tesla.getUpperBand(data)
+lower = tesla.getLowerBand(data)
 tesla.getGraph(data)
