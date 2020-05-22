438b9e324ec986ca1a79b1e3f530368955050de3
Matthew Schreider <mSchreider@ips-yes.com>
2020-05-22 15:59:21 -0400

    First attempt at adding a cursor that snaps to line

--- data.py
+++ data.py
@@ -34,25 +34,12 @@ class Stock():
 
         return df
     
-    def getUpperBand(self, df):
+    def getBollBand(self, df):
         sma = df['20-day SMA'].tolist()
         time_period = 20
-        """
-        numbers_series = pd.Series(sma)
-        std_dev = numbers_series.rolling(time_period).std()  
-        std_dev_list = std_dev.tolist()
-        df['20-Day Standard Deviation'] = std_dev_list
-        
-        upper = sma + df['Close'].rolling(time_period).std() * 2
-        df['upperband'] = upper
-        print(upper)
-        """
+        df['Lower Band'] = df['20-day SMA'] - df['Close'].rolling(time_period).std() * 2
         df['Upper Band'] = df['20-day SMA'] + df['Close'].rolling(time_period).std() * 2
-        return df 
-
-    def getLowerBand(self, df):
-        sma = df['20-day SMA'].tolist()
-        time_period = 20
+        return df
         """
         numbers_series = pd.Series(sma)
         std_dev = numbers_series.rolling(time_period).std()  
@@ -63,8 +50,6 @@ class Stock():
         df['lowerband'] = lower
         print(lower)
         """
-        df['Lower Band'] = df['20-day SMA'] - df['Close'].rolling(time_period).std() * 2
-        return df 
 
     def inc_dec(self, c, o):
         if c > o:
@@ -74,53 +59,80 @@ class Stock():
         else:
             value = "Equal"
         return value
+
+class SnapToCursor(object):
+    def __init__(self, ax, x, y):
+        self.ax = ax
+        self.lx = ax.axvline(color='k') # The horizontal Line
+        self.ly = ax.axvline(color='k') # The Vertcal Line
+        self.x = x
+        self.y = y
+        # Text location in axes coords
+        self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
     
-    def getGraph(self, df):
+    def mouse_move(self, event):
+        if not event.inaxes:
+            return
         
-        df["Status"] = [self.inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
-        df["Middle"] = (df.Open + df.Close) / 2
-        df["Daily Change"] = abs(df.Close - df.Open)
-        date = df.index.values
-        openprice = df['Open']
-        closeprice = df['Close']
-        high = df['High']
-        low = df['Low']
+        x, y = event.xdata, event.ydata
+        indx = min(np.searchsorted(self.x, x), len(self.x) -1)
+        x = self.x[indx]
+        y = self.y[indx]
+        # update the line positions
+        self.lx.set_ydata(y)
+        self.ly.set_xdata(x)
 
-        ## Get data for candlestick_ochl() ##
-        x = 0
-        y = len(date)
-        data = []
-        while x < y:
-            append_me = date[x], openprice[x], closeprice[x], high[x], low[x]
-            data.append(append_me)
-            x += 1
-        
-        ## Plot Candlesticks ##
-        fig = pyplot.figure(figsize=(10, 7))
-        ax1 = pyplot.subplot()
-        candlestick_ochl(ax1, data, width=np.timedelta64(12, 'h'), colorup='green', colordown='red', alpha=0.75)
-        ## Plot 20-Day SMA overlapping Candlestick plot ##
-        sma_plot = pyplot.subplot()
-        pyplot.plot(df['20-day SMA'], color='#26baee', linestyle='--')  
+        self.txt.set_text('x=%1.2f, y=%1.2f' % (x, y))
+        print('x=%1.2f, y=%1.2f' % (x, y))
+        self.ax.figure.canvas.draw()
 
-        ## Plot Upper Bands ##
-        upper_plot = pyplot.subplot()
-        pyplot.plot(df['Upper Band'], color='#783eff', linestyle='-')
-        
-        ## Plot Lower Bands ##
-        lower_plot = pyplot.subplot()
-        pyplot.plot(df['Lower Band'], color='#783eff', linestyle='-')
-
-        ## Set Axis properties ##
-        pyplot.yticks(np.arange(min(closeprice)-(0.1*min(closeprice)), max(closeprice)+(0.1*max(closeprice)),  50))
-        pyplot.ylabel('Price')
-        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
-        pyplot.xticks(rotation=45)
-        pyplot.xlabel('Date')
-        pyplot.show()
-        
+    
+## Initialize Stock() Class ##
 tesla = Stock('TSLA')
 data = tesla.getSMA()
-upper = tesla.getUpperBand(data)
-lower = tesla.getLowerBand(data)
-tesla.getGraph(data)
+df = tesla.getBollBand(data)
+
+## Extract data from tesla dataframe ##
+#df["Status"] = [tesla.inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
+#df["Middle"] = (df.Open + df.Close) / 2
+df["Daily Change"] = abs(df.Close - df.Open)
+date = df.index.values
+openprice = df['Open']
+closeprice = df['Close']
+high = df['High']
+low = df['Low']
+
+## Get data for candlestick_ochl() and append to a list ##
+x = 0
+y = len(date)
+data = []
+while x < y:
+    append_me = date[x], openprice[x], closeprice[x], high[x], low[x]
+    data.append(append_me)
+    x += 1
+print(type(date[1]))
+## Plot Candlesticks ##
+fig, ax1 = pyplot.subplots()
+candlestick_ochl(ax1, data, width=np.timedelta64(12, 'h'), colorup='green', colordown='red', alpha=0.75)
+## Plot 20-Day SMA overlapping Candlestick plot ##
+ax1.plot(df['20-day SMA'], color='#26baee', linestyle='--')  
+
+## Plot Upper Bands ##
+ax1.plot(df['Upper Band'], color='#783eff', linestyle='-')
+
+## Plot Lower Bands ##
+ax1.plot(df['Lower Band'], color='#783eff', linestyle='-')
+
+## Snap to Cursor ##
+#snap_cursor = SnapToCursor(ax1, date, closeprice)
+#fig.canvas.mpl_connect('motion_notify_event', snap_cursor.mouse_move)
+
+## Set Plot properties ##
+fig.suptitle('Bollinger Bands of TESLA')
+pyplot.yticks(np.arange(min(closeprice)-(0.1*min(closeprice)), max(closeprice)+(0.1*max(closeprice)),  50))
+pyplot.ylabel('Price')
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+fig.autofmt_xdate()
+pyplot.xticks(rotation=45)
+pyplot.xlabel('Date')
+pyplot.show()
